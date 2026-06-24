ef3564f2f1dcd6558c25dd8387d50867fb69a58b
ZoeAby <zoe.damore@estudiantes.unahur.edu.ar>
2026-06-24 04:33:47 -0300

    agrego el CommentRepository

--- src/repositories/CommentRepository.js
+++ src/repositories/CommentRepository.js
@@ -1,15 +1,32 @@
 const { Comment } = require('../models');
 
 class CommentRepository {
-    async create(commentData) {}
+    async create(commentData) {
+        return Comment.create(commentData);
+    }
 
-    async findByContent(content) {}
+    async findByContent(content) {
+        return Comment.find({
+            content: { $regex: content, $options: 'i' }
+        }).lean();
+    }
 
-    async findAll() {}
+    async findAll() {
+        return Comment.find({}).lean();
+    }
 
-    async update(id, commentData) {}
+    async update(id, commentData) {
+        const comment = await Comment.findById(id);
 
-    async delete(id) {}
+        if (!comment) return null;
+
+        Object.assign(comment, commentData);
+        return await comment.save();
+    }
+
+    async deleteById(id) {
+        return Comment.deleteOne({ _id: id });
+    }
 }
 
 module.exports = new CommentRepository();
