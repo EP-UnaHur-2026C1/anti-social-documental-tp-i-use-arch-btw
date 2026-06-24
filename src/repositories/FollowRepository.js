a690d3575537b50e58b6c12de6af062035302ee2
ZoeAby <zoe.damore@estudiantes.unahur.edu.ar>
2026-06-24 04:37:18 -0300

    actualizacion: FollowRepository

--- src/repositories/FollowRepository.js
+++ src/repositories/FollowRepository.js
@@ -1,11 +1,38 @@
 const { Follow } = require('../models');
 
 class FollowRepository {
-    async create(followerNickName, followingNickName) {}
+    async create(followerNickName, followingNickName) {
+        return Follow.create({
+            follower_nickName: followerNickName,
+            following_nickName: followingNickName
+        });
+    }
 
-    async delete(followerNickName, followingNickName) {}
+    async delete(followerNickName, followingNickName) {
+        return Follow.deleteOne({
+            follower_nickName: followerNickName,
+            following_nickName: followingNickName
+        });
+    }
 
-    async findOne(followerNickName, followingNickName) {}
+    async findOne(followerNickName, followingNickName) {
+        return Follow.findOne({
+            follower_nickName: followerNickName,
+            following_nickName: followingNickName
+        }).lean();
+    }
+
+    async findFollowers(nickName) {
+        return Follow.find({
+            following_nickName: nickName
+        }).lean();
+    }
+
+    async findFollowing(nickName) {
+        return Follow.find({
+            follower_nickName: nickName
+        }).lean();
+    }
 }
 
 module.exports = new FollowRepository();
